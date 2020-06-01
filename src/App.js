7f0ede852c195fe450287406183b36466228130c
PD2019 <jadhavv933@gmail.com>
2020-06-01 16:04:59 +0530

    Created Single Card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import PostList from "./components/PostList";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
+
+  // componentDidMount() {
+  //   fetch("https://www.reddit.com/.json")
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ posts: data.data.children }));
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Reddit</h1>
+        <PostList />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SimplePost from "./SimplePost";
+
+export default function PostList() {
+  return (
+    <div>
+      <SimplePost />
+    </div>
+  );
+}
